97f5d2631bccf9d991801d3e2933c81b062fab3b
Clint Moran <cmoran@healthwise.org>
2019-12-12 14:42:12 -0700

    Derek did some stuff

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
-import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Header from "./Pages/Layout/Header";
+import Footer from "./Pages/Layout/Footer";
 import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
+      <Header />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
     </Router>
   );
 }
